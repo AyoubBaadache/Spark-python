da0749a8f4fdd049a7d6d480bd967ab27df61379
Balder221 <ayoubelegend@gmail.com>
2024-03-25 12:25:27 +0100

    Spark (python) v 1.0

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+ print("Press the green button")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
