afabaaa7e1f021cba304da1e0fcfa4489a937b23
Balder221 <ayoubelegend@gmail.com>
2024-03-27 14:40:00 +0100

    Spark (python) v 3.1 (* Adding a Docker file to upload the jars automatically                       * Note: that execution time increased using this method)

--- Job_submit_without.packaging.py
+++ Job_submit_without.packaging.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     master_url = 'spark://spark-master:7077'
     jars = ('/opt/bitnami/spark/resource/mongo-spark-connector_2.12-3.0.1.jar,'
             '/opt/bitnami/spark/resource/mongo-java-driver-3.12.8.jar')
-    app_script_path = '/opt/bitnami/spark/jobs/My_Job.py'
+    app_script_path = '/opt/bitnami/spark/jobs/Mongo_Job.py'
 
     submit_spark_job_via_spark_submit(master_url, jars, app_script_path)
